8536f9ee016b044e6884426906dfdad6d219b618
zapeture <64714576+zapeture@users.noreply.github.com>
2021-01-08 19:52:26 +0100

    "third commit"

--- js/main.js
+++ js/main.js
@@ -21,3 +21,49 @@
        })
 
 })();
+
+// portfolio filter and popup
+(() =>{
+
+ 
+const filterContainer = document.querySelector(".portfolio-filter"),
+portfolioItemsContainer = document.querySelector(".portfolio-items"),
+portfolioItems = document.querySelectorAll(".portfolio-item"),
+popup = document.querySelector(".portfolio-popup"),
+prevBtn = popup.querySelector(".pp-prev"),
+nextBtn = popup.querySelector(".pp-next"),
+closeBtn = popup.querySelector(".pp-close"),
+projectDetailsContainer = popup.querySelector(".pp-project-details-btn");  
+let itemIndex, slideIndex, screenshots;
+
+// filterportfolionitems
+filterContainer.addEventListener("click", (event) => {
+
+  if(event.target.classList.contains("filter-item") && 
+  !event.target.classList.contains("active"))
+  {// deactivate existing active filter item
+    filterContainer.querySelector(".active").classList.remove("outer-shadow","active");
+    //activate new 'filter';
+    event.target.classList.add("active","outer-shadow");
+    const target = event.target.getAttribute("data-target");
+    portfolioItems.forEach((item)=>{
+     
+      if(target === item.getAttribute("data-category") || target === 'all'){
+        item.classList.remove("hide");
+        item.classList.add("show");
+      }
+      else{
+        item.classList.remove("show");
+        item.classList.add("hide");
+
+      }
+    })
+  }
+  
+
+}
+
+)
+
+
+})();
