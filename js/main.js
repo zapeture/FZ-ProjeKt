3f8a28a73295226dfe7419654b56dd728481c6c9
Fortune Zviregei <contact@fortunezviregei.com>
2021-01-10 17:59:58 +0100

    "sixth commit"

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-// about section tabs
+
+
+//--------------------------- about section tabs
 (() =>{
 
       const aboutSection = document.querySelector(".about-section"),
@@ -191,3 +193,58 @@ portfolioItemsContainer.addEventListener("click", (event) =>{
     }
 
 })();
+
+//-------------------Testomonial--Slider----------------------//
+
+(() =>{
+
+  const sliderContainer = document.querySelector(".testi-slider-container"),
+  slides = sliderContainer.querySelectorAll(".testi-item"),
+  slideWidth = sliderContainer.offsetWidth;
+  prevBtn = document.querySelector(".testi-slider-nav .prev"),
+  activeSlide = sliderContainer.querySelector(".testi-item.active");
+  nextBtn = document.querySelector(".testi-slider-nav .next");
+  let slideIndex = Array.from(activeSlide.parentElement.children).indexOf(activeSlide);
+
+
+  // set width of all slides
+
+  slides.forEach((slide) =>{
+    slide.style.width = slideWidth + "px";
+  })
+  // set width of sliderContainer
+  sliderContainer.style.width = slideWidth * slides.length + "px";
+
+  nextBtn.addEventListener("click",() =>{
+     if(slideIndex === slides.length-1){
+       slideIndex = 0;
+     }
+     else{
+       slideIndex++;
+     }
+     slider();
+
+  })
+
+  prevBtn.addEventListener("click", () =>{
+    if(slideIndex === 0){
+
+     slideIndex = slides.length-1;
+    }
+    else{
+     slideIndex--;
+   }
+     slider();
+   })
+    
+   function slider(){
+     // deactivate existing active slides
+     sliderContainer.querySelector(".testi-item.active").classList.remove("active");
+     // activate new slide
+     slides[slideIndex].classList.add("active");
+    sliderContainer.style.marginLeft = - (slideWidth * slideIndex) + "px";
+
+   }
+   slider();
+})();
+
